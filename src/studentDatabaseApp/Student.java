21c1a6fd2cdc926311df82216d1f57b8f05ddf6c
Theshan Kaveen <64361534+tkaveen@users.noreply.github.com>
2021-08-03 18:47:56 +0530

    variable initialization

--- src/studentDatabaseApp/Student.java
+++ src/studentDatabaseApp/Student.java
@@ -1,5 +1,12 @@
 package studentDatabaseApp;
 
 public class Student {
+    private String firstName;
+    private String lastName;
+    private String year;
+    private int studentID;
+    private String courses;
+    private int tuitionBalance;
+    private int costOfCourse = 600;
 
 }
