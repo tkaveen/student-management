4b6fc63f0136a8d5785a564e3dbbd8aab9a4b0e0
Theshan Kaveen <64361534+tkaveen@users.noreply.github.com>
2021-08-05 23:18:03 +0530

    Student ID generate done

--- src/studentDatabaseApp/Student.java
+++ src/studentDatabaseApp/Student.java
@@ -6,12 +6,13 @@ public class Student {
     private String firstName;
     private String lastName;
     private int year;
-    private int studentID;
+    private String studentID;
     private String courses;
     private int tuitionBalance;
     private int costOfCourse = 600;
-    private static int id = 10000;
+    private static int id = 1000;
 
+    //Constructor : Get student's f name and l name
     public Student() {
         Scanner in = new Scanner(System.in);
         System.out.println("Enter Student First Name : ");
@@ -22,10 +23,19 @@ public class Student {
 
         System.out.println("1 - 1st Year\n2 - 2nd Year\n3 - 3rd Year\n4 - 4th Year\nEnter Student Year : ");
         this.year = in.nextInt();
-        System.out.println(firstName+ " " + lastName + " " + year);
 
+        setStudentId();
+
+        System.out.println(firstName + " " + lastName + " " + year +  " " +studentID);
+
+
+
+    }
+
+    //Generate student ID
+    private void setStudentId() {
         id++;
-
+        studentID = year + "" + id;
     }
 
 }
