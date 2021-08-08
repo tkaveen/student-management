fb700276b4d207d2cd499f4c077d380aed01a148
Theshan Kaveen <64361534+tkaveen@users.noreply.github.com>
2021-08-08 20:55:21 +0530

    Final commit

--- src/studentDatabaseApp/StudentDatabaseApp.java
+++ src/studentDatabaseApp/StudentDatabaseApp.java
@@ -1,11 +1,26 @@
 package studentDatabaseApp;
 
+import java.util.Scanner;
+
 public class StudentDatabaseApp {
     public static void main(String[] args) {
-        Student st1 = new Student();
-        st1.enroll();
-        st1.payTuition();
-        System.out.println(st1.toString());
+
+
+        //Ask how many students wish to add
+        System.out.println("Enter Number of Students want to add : ");
+        Scanner in = new Scanner(System.in);
+        int num = in.nextInt();
+        Student[] students = new Student[num];
+
+        //Create students
+        for (int n = 0; n < num; n++) {
+            students[n] = new Student();
+            students[n].enroll();
+            students[n].payTuition();
+        }
+        for (int n = 0; n < num; n++) {
+            System.out.println(students[n].toString());
+        }
 
     }
 }
